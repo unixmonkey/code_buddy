b2facc8d3239498e89e7751e8c8d0f5659f1d7fb
David Jones <unixmonkey1@gmail.com>
2014-04-17 22:59:45 -0400

    Add extra stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super
-    end
-  end
-
-
   protected
 
   def configure_permitted_parameters
-
-    devise_parameter_sanitizer.for(:user) << :first_name
-  
-    devise_parameter_sanitizer.for(:user) << :last_name
-    devise_parameter_sanitizer.for(:user) << :interests
-
-    
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
+    devise_parameter_sanitizer.for(:account_update) << :interests
   end
-  
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
 
   def show
-      @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
   end
+
   def index
-      @users = User.all
+    @users = User.all
   end
   
   private
+
   def user_params
-      params.require(:user).permit(:first_name, :last_name, :interests)
+    params.require(:user).permit(:first_name, :last_name, :interests)
   end
 
 end
