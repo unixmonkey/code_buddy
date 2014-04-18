20a8152e05d031c4f44f52f3d8aa4ea202151442
David Jones <unixmonkey1@gmail.com>
2014-04-17 23:08:53 -0400

    Remove unneccesary user_params
    The params are handled by devise, and the extra stuff in
    application_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,5 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :interests)
-  end
 
 end
